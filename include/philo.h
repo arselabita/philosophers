3ef1d7c4a842e80c6b034db1e11ce8904dc25384
Arsela Bita <arselabita@gmail.com>
2025-10-21 15:35:15 +0200

    Merge remote dev with local dev

--- include/philo.h
+++ include/philo.h
@@ -13,17 +13,19 @@
 
 #ifndef PHILOS_H
 # define PHILOS_H
-# define ALLOCATING_FAILED 1
+# define ALLOCATING_FAILED 2
+# define INVALID_PHILOS 1
+# define FAILED_CREATING_THREADS 3
+# define FAILED_JOINING_THREADS 4
 
 # include <stdio.h> // printf() funct
 # include <unistd.h> // for sleep funct
 # include <pthread.h> // POSIX thread library
 # include <stdlib.h> // malloc, free funct
-# include "libft.h" // libft helper funct
 
 typedef struct s_philo
 {
-	pthread_t thread;
+	pthread_t *thread;
 	
 } t_philo;
 
--- src/main.c
+++ src/main.c
@@ -15,35 +15,44 @@
 // playing around with threads
 // how to create a thread? -> we are going to use 
 
+void *routine(void *arg)
+{
+	int philo_index;
+
+	philo_index = *(int *)arg;
+	printf("Philosofer %d\n", philo_index);
+	free(arg);
+	
+}
+
 int main(int argc, char **argv)
 {
-	t_philo *philos;
+	t_philo philos;
 	int conv_philos_num;
 	int i;
 
 	if (argc != 2)
 		return (EXIT_FAILURE);
-	conv_philos_num = ft_atoi(argv[1]);
-	philos = ft_calloc(conv_philos_num, sizeof(t_philo));
-	if (!philos)
+	conv_philos_num = atoi(argv[1]);
+	if (conv_philos_num <= 0)
+		return(perror("Invalid number of philosophers.\n"), INVALID_PHILOS);
+	philos.thread = calloc(conv_philos_num, sizeof(pthread_t));
+	if (!philos.thread)
 		return (ALLOCATING_FAILED);
-	
-
-
+	i = 0;
+	while(i < conv_philos_num)
+	{
+		if(pthread_create(philos.thread + i, NULL, &routine, i) != 0)
+			return (perror("Failed to create thread.\n"), FAILED_CREATING_THREADS);
+		i++;
+	}
+	i = 0;
+	while (i < conv_philos_num)
+	{
+		if (pthread_join(philos.thread[i], NULL) != 0)
+			return (perror("Failed to join threads.\n"), FAILED_JOINING_THREADS);
+		printf("Thread %d has finished execution.\n");
+		i++;
+	}
 	return (0);
 }
-	// pthread_t thread1;
-	// pthread_t thread2;
-	// pthread_mutex_init(&mutex, NULL);
-
-	// if (pthread_create(&thread1, NULL, &rutine, NULL) != 0)
-	// 	return 1;
-	// if (pthread_create(&thread2, NULL, &rutine, NULL) != 0)
-	// 	return 2;
-	// // wait until it has finished executing
-	// if (pthread_join(thread1, NULL) != 0)
-	// 	return 3;
-	// if (pthread_join(thread2, NULL) != 0)
-	// 	return 4;
-	// pthread_mutex_destroy(&mutex);
-	// printf("Number of mails: %d\n", mails);
