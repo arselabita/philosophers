1108a6a69b0b209474414b626156b4a94f7a8afb
Arsela Bita <arselabita@gmail.com>
2025-10-24 20:15:49 +0200

    a working version

---
+++ philo/include/philo.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 14:45:02 by abita             #+#    #+#             */
+/*   Updated: 2025/10/14 14:45:05 by abita            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOS_H
+# define PHILOS_H
+# define ALLOCATING_FAILED 1
+# define INVALID_PHILOS 1
+# define FAILED_CREATING_THREADS 1
+# define FAILED_JOINING_THREADS 1
+# define ERR_PHILOS_FUNCT 1
+
+# include <stdio.h> // printf() funct
+# include <unistd.h> // for sleep funct
+# include <pthread.h> // POSIX thread library
+# include <stdlib.h> // malloc, free funct
+# include <limits.h> // size_t, INT_MAX, INT_MIN
+#include <stdint.h> // SIZE_MAX
+
+typedef struct s_mutex
+{
+	pthread_mutex_t printing;
+} t_mutex;
+
+typedef struct s_philo
+{
+	int index;
+	pthread_t thread;
+	pthread_mutex_t printing;
+} t_philo;
+
+void	*ft_calloc(size_t count, size_t size);
+int		ft_atoi(const char *nptr);
+void	ft_bzero(void *s, size_t n);
+
+#endif
---
+++ philo/src/main.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 11:07:50 by abita             #+#    #+#             */
+/*   Updated: 2025/10/24 19:45:17 by abita            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philo.h"
+
+// playing around with threads
+// how to create a thread? -> we are going to use 
+
+static void *start_routine(void *arg)
+{
+	t_philo *philo;
+
+	philo = (t_philo *)arg;
+	pthread_mutex_lock(&philo->printing);
+	printf("Philosofer %d\n", philo->index);
+	pthread_mutex_unlock(&philo->printing);
+	return (NULL);
+}
+
+int main(int argc, char **argv)
+{
+	t_philo *philos;
+	int convert_to_number;
+	int i;
+ 
+	if (argc != 2)
+		return (EXIT_FAILURE);
+	if ((convert_to_number = ft_atoi(argv[1])) <= 0)
+		return(perror("Invalid number of philosophers.\n"), INVALID_PHILOS);
+	philos = ft_calloc(convert_to_number, sizeof(t_philo));
+	if (!philos)
+		return (ALLOCATING_FAILED);
+	i = 0;
+	pthread_mutex_init(&philos->printing, NULL);
+	while (i < convert_to_number)
+	{
+		philos[i].index = i + 1;
+		if(pthread_create(&philos[i].thread, NULL, &start_routine, &philos[i]) != 0)
+			return (perror("Failed to create thread.\n"), FAILED_CREATING_THREADS);
+		i++;
+	}
+	usleep(100);
+	i = 0;
+	while (i < convert_to_number)
+	{
+		if (pthread_join(philos[i].thread, NULL) != 0)
+			return (perror("Failed to join threads.\n"), FAILED_JOINING_THREADS);
+		printf("Thread %d has finished execution.\n", philos[i].index);
+		i++;
+	}
+	pthread_mutex_destroy(&philos->printing);
+	free(philos);
+	return (0);
+}
