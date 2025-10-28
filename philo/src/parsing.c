ef64e13f128209330619014d8c428428fead357e
Arsela Bita <arselabita@gmail.com>
2025-10-28 11:50:22 +0100

    parsing_arguments: separated it in sub-helper funct

--- philo/src/parsing.c
+++ philo/src/parsing.c
@@ -32,6 +32,33 @@ static int	parsing_helper(char *nums, int *number)
 	return (EXIT_SUCCESS);
 }
 
+int arg_check(int argc, char **argv, int number, t_data *data)
+{
+	if (argc == 6)
+	{
+		if (parsing_helper(argv[5], &number))
+			return (EXIT_FAILURE);
+		data->number_of_times_each_philosopher_must_eat = number;
+	}
+	else
+		data->number_of_times_each_philosopher_must_eat = -1;
+	printf("\n**********DEBUGING*************\n");
+	printf (" num of philos: %d\n time to die: %d\n time to eat: %d\n time to sleep: %d\n num of times ph eat: %d\n", data->num_of_philos, data->time.time_to_die, data->time.time_to_eat, data->time.time_to_sleep, data->number_of_times_each_philosopher_must_eat);
+	printf("*******************************\n");
+	if (data->num_of_philos <= 0 || data->time.time_to_die <= 0 \
+		|| data->time.time_to_eat <= 0 || data->time.time_to_sleep <= 0)
+		{
+			print_error("Error: All arguments must be greater than 0.\n");
+			return (EXIT_FAILURE);
+		}
+	if (argc == 6 && data->number_of_times_each_philosopher_must_eat <= 0)
+	{
+		print_error("Error: number_of_times_each_philosopher_must_eat must be > than 0.\n");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
 int	parse_arguments(int argc, char **argv, t_data *data)
 {
 	int		i;
@@ -54,39 +81,7 @@ int	parse_arguments(int argc, char **argv, t_data *data)
 		data->time.time_to_sleep = number;
 		i++;
 	}
-	if (argc == 6)
-	{
-		if (parsing_helper(argv[5], &number))
-			return (EXIT_FAILURE);
-		data->number_of_times_each_philosopher_must_eat = number;
-	}
-	else
-		data->number_of_times_each_philosopher_must_eat = -1;
-
-	printf("**********DEBUGING*************\n");
-	printf (" num of philos: %d\n time to die: %d\n time to eat: %d\n time to sleep: %d\n num of times ph eat: %d\n", data->num_of_philos, data->time.time_to_die, data->time.time_to_eat, data->time.time_to_sleep, data->number_of_times_each_philosopher_must_eat);
-	printf("*******************************\n");
-	
-	
-	if (data->num_of_philos <= 0 || data->time.time_to_die <= 0 \
-		|| data->time.time_to_eat <= 0 || data->time.time_to_sleep <= 0)
-		{
-			print_error("Error: All arguments must be greater than 0.\n");
-			return (EXIT_FAILURE);
-		}
-	if (argc == 6 && data->number_of_times_each_philosopher_must_eat <= 0)
-	{
-		print_error("Error: number_of_times_each_philosopher_must_eat must be > than 0.\n");
-		return (EXIT_FAILURE);
-	}		
+	if (arg_check(argc, argv, number, data) == EXIT_FAILURE)
+		return (EXIT_FAILURE);		
 	return (EXIT_SUCCESS);
 }
-/*
-	if (argc != 2)
-		return (EXIT_FAILURE);
-	if ((philo. = ft_atoi(argv[1])) <= 0)
-		return(perror("Invalid number of philosophers.\n"), INVALID_PHILOS);
-	*philo = ft_calloc(data->conv_to_num, sizeof(t_philo));
-	if (!philo)
-		return (ERR_ALLOCATING);
-*/
