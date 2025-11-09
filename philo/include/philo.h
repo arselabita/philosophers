97a03228edb029952aa4951e2637e900b5b5f9d4
Arsela Bita <arselabita@gmail.com>
2025-11-09 15:32:07 +0100

    Created enum to handle all the errors, also added the definitions of each color for the routine.

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -12,74 +12,88 @@
 
 #ifndef PHILOS_H
 # define PHILOS_H
-# define ERR_ALLOCATING 1
-# define INVALID_PHILOS 1
-# define ERR_CREATING_THREADS 1
-# define ERR_JOINING_THREADS 1
-# define ERR_PHILOS_FUNCT 1
-# define ERR_GET_TIME 1
-# define ERR_PARSING 1
 
-# include <stdio.h> // printf() funct
-# include <unistd.h> // for sleep funct
-# include <pthread.h> // POSIX thread library
-# include <stdlib.h> // malloc, free funct
-# include <limits.h> // size_t, INT_MAX, INT_MIN
-# include <stdint.h> // SIZE_MAX
+# define GREEN "\033[32m"
+# define BLUE "\033[34m"
+# define YELLOW "\033[33m"
+# define CYAN "\033[36m"
+# define RED "\033[31m"
+# define RESET_COLOR "\033[0m"
+
+# include <limits.h>   // size_t, INT_MAX, INT_MIN
+# include <pthread.h>  // POSIX thread library
+# include <stdint.h>   // SIZE_MAX
+# include <stdio.h>    // printf() funct
+# include <stdlib.h>   // malloc, free funct
 # include <sys/time.h> // gettimeofday
+# include <unistd.h>   // for sleep funct
 
-typedef struct s_data t_data;
-typedef struct s_time t_time;
-typedef struct s_philo t_philo;
+typedef struct s_data	t_data;
+typedef struct s_time	t_time;
+typedef struct s_philo	t_philo;
 
 // time related values in here
 typedef struct s_time
 {
-	int time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-} t_time;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+}						t_time;
 
 // all global simulation settings
 typedef struct s_data
 {
-	int num_of_philos;
-	int number_of_times_each_philosopher_must_eat;
-	long start_time;
-	pthread_mutex_t printing;
-	pthread_mutex_t *fork;
-	t_time time;
-} t_data;
+	int					num_of_philos;
+	int					number_of_times_each_philosopher_must_eat;
+	long				start_time;
+	pthread_mutex_t		printing;
+	pthread_mutex_t		*fork;
+	t_time				time;
+}						t_data;
 
 // and the philo struct
 typedef struct s_philo
 {
-	pthread_t philo_thread;
-	int philo_id;
-	int not_dead;
-	pthread_mutex_t *right_fork;
-	pthread_mutex_t *left_fork;
-	t_data *data;
+	pthread_t			philo_thread;
+	int					philo_id;
+	int					not_dead;
+	pthread_mutex_t		*right_fork;
+	pthread_mutex_t		*left_fork;
+	t_data				*data;
 
-} t_philo;
+}						t_philo;
+
+// enum for all the errors
+typedef enum e_error
+{
+	SUCCESS = 0,
+	ERR_ALLOCATING,
+	ERR_INVALID_PHILOS,
+	ERR_CREATING_THREADS,
+	ERR_JOINING_THREADS,
+	ERR_PHILOS_FUNCT,
+	ERR_GET_TIME,
+	ERR_PARSING
+}						t_error;
 
 // utils
-void	*ft_calloc(size_t count, size_t size);
-long long	ft_atol(const char *nptr);
-void	ft_bzero(void *s, size_t n);
-int		ft_valid_number(char *str);
-void	print_error(const char *msg);
-void 	print_msg(t_philo *philo, char *msg);
+void					*ft_calloc(size_t count, size_t size);
+long long				ft_atol(const char *nptr);
+void					ft_bzero(void *s, size_t n);
+int						ft_valid_number(char *str);
+void					print_error(const char *msg);
+void					print_msg(t_philo *philo, char *msg);
+int						ft_strcmp(char *s1, char *s2);
 
 // parsing
-int 	parse_arguments(int argc, char **argv, t_data *data);
+int						parse_arguments(int argc, char **argv, t_data *data);
 
 // simulation
-int 	init_run_thread(t_data *data, t_philo **philo);
-int		philo_routine(t_philo *philo);
+int						init_run_thread(t_data *data, t_philo **philo);
+int						philo_routine(t_philo *philo);
 
 // time
-long	getmillisec();
-long 	calc_time(t_data *data);
+long					getmillisec(void);
+long					calc_time(t_data *data);
 
 #endif
