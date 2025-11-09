3da09fd54357275713e333fe3b16998959fd1ddd
Arsela Bita <arselabita@gmail.com>
2025-11-09 15:33:10 +0100

    called and declared the enum and assigned it to the functions who return

--- philo/src/main.c
+++ philo/src/main.c
@@ -13,34 +13,35 @@
 #include "../include/philo.h"
 
 /////////////////////
-// the dining philosophers problem is an example problem 
-// often used in concurrent algorithm design to illustrate synchronization 
+// the dining philosophers problem is an example problem
+// often used in concurrent algorithm design to illustrate synchronization
 // issues and techniques for resolving them.
 /////////////////////
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_data  data;
-	t_philo *philo;
+	t_error	result;
+	t_data	data;
+	t_philo	*philo;
 
 	philo = NULL;
 	if (argc < 5 || argc > 6)
 	{
 		print_error("Error: Invalid number of arguments.\n");
-		print_error("Usage: ./philo [number_of_philosophers] [time_to_die] [time_to_eat] [time_to_sleep] [number_of_times_each_philosopher_must_eat]\n");
+		print_error("Usage: ./philo [number_of_philosophers] [time_to_die] \
+			[time_to_eat] [time_to_sleep] \
+			[number_of_times_each_philosopher_must_eat]\n");
 		return (EXIT_FAILURE);
 	}
-	if (parse_arguments(argc, argv, &data) != EXIT_SUCCESS)
-	{
-		print_error("Error: Failed to parse arguments.\n");
-		return (ERR_PARSING);
-	}
+	result = parse_arguments(argc, argv, &data);
+	if (result != SUCCESS)
+		return (print_error("Error: Failed to parse arguments.\n"),
+			ERR_PARSING);
 	pthread_mutex_init(&data.printing, NULL);
 	data.start_time = getmillisec();
-	if (init_run_thread(&data, &philo) != EXIT_SUCCESS)
-	{
-		print_error("Error: Failed to initialize philos.\n");
-		return (INVALID_PHILOS);	
-	}
+	result = init_run_thread(&data, &philo);
+	if (result != SUCCESS)
+		return (print_error("Error: Failed to initialize philos.\n"),
+			ERR_INVALID_PHILOS);
 	return (EXIT_SUCCESS);
 }
