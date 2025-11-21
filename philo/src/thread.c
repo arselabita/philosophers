ad3f787e1c360522efa9834f65794148b9b603f3
Arsela Bita <arselabita@gmail.com>
2025-11-21 21:17:33 +0100

    protected the threads and added the death and join flag in case it dies or it doesnt so that it can join in both situations

--- philo/src/thread.c
+++ philo/src/thread.c
@@ -6,7 +6,7 @@
 /*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 12:35:34 by abita             #+#    #+#             */
-/*   Updated: 2025/10/27 12:35:35 by abita            ###   ########.fr       */
+/*   Updated: 2025/11/21 21:11:28 by abita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,40 +31,36 @@ static void	init_philos_thread(t_data *data, t_philo *philo)
 	{
 		philo[i].philo_id = i + 1;
 		philo[i].data = data;
+		philo[i].meals_count = 0;
 		pthread_mutex_lock(&data->dead_mutex);
 		philo[i].last_meal = data->start_time;
 		pthread_mutex_unlock(&data->dead_mutex);
 		if (pthread_create(&philo[i].philo_thread, NULL, &philo_thread_start,
 				&(philo)[i]) != 0)
 		{
+			philo->data->stop_flag = true;
+			data->join_check = true;
 			print_error("Failed to create philosopher thread.\n");
 			j = 0;
 			while (j < i)
 				pthread_join(philo[j++].philo_thread, NULL);
-			free(philo);
+			return ;
 		}
 		i++;
 	}
-	usleep(1);
 }
 
 static void	join_philo_thread(t_data *data, t_philo *philo)
 {
 	int	i;
-	int	j;
 
 	i = 0;
 	while (i < data->num_of_philos)
 	{
 		if (pthread_join(philo[i].philo_thread, NULL) != 0)
 		{
-			j = 0;
-			while (j < data->num_of_philos)
-			{
-				pthread_join(philo[j].philo_thread, NULL);
-				j++;
-			}
-			free(philo);
+			print_error("Failed to join philosopher thread.\n");
+			return ;
 		}
 		i++;
 	}
@@ -73,14 +69,15 @@ static void	join_philo_thread(t_data *data, t_philo *philo)
 int	init_run_thread(t_data *data, t_philo **philo)
 {
 	data->stop_flag = false;
+	data->join_check = false;
 	*philo = ft_calloc(data->num_of_philos, sizeof(t_philo));
 	if (!*philo)
 		return (ERR_ALLOCATING);
 	data->philo = *philo;
 	init_philos_thread(data, *philo);
-	init_waiter_thread(data, *philo);
-	join_philo_thread(data, *philo);
-	join_waiter_thread(data, *philo);
+	init_join_waiter_thread(data, *philo);
+	if (!data->join_check)
+		join_philo_thread(data, *philo);
 	clean_up(data, *philo);
 	return (EXIT_SUCCESS);
 }
