797b1a9099d7d1fc4fa9587adfafc90d1734a592
Arsela Bita <arselabita@gmail.com>
2025-11-21 21:18:14 +0100

    added a check for death in my_usleep in case we pass a long time to make it stop

--- philo/src/time.c
+++ philo/src/time.c
@@ -6,7 +6,7 @@
 /*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 14:02:01 by abita             #+#    #+#             */
-/*   Updated: 2025/11/04 14:02:03 by abita            ###   ########.fr       */
+/*   Updated: 2025/11/21 21:11:35 by abita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,20 @@ long getmillisec()
 	if (gettimeofday(&tv, NULL) == -1)
 		return (print_error("Failed to give time of day.\n"), ERR_GET_TIME);
 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
-	// Convering seconds to milliseconds and microseconds too
 }
 long calc_time(t_data *data)
 {
 	return (getmillisec() - data->start_time);
 }
 
-void my_usleep(uint64_t time)
+void my_usleep(uint64_t time, t_philo *philo)
 {
 	uint64_t start;
 	
 	start = getmillisec();
-	while (getmillisec() - start < time)
-		usleep(500);
+	while (!philo->data->stop_flag)
+	{
+		if (getmillisec() - start < time)
+			usleep(500);
+	}
 }
