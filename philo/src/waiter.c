700f2c6d0c0682db0240435fc4cae4f073405925
Arsela Bita <arselabita@gmail.com>
2025-11-21 21:18:58 +0100

    separated the monitoring also added checks if it fails in the creation

--- philo/src/waiter.c
+++ philo/src/waiter.c
@@ -6,26 +6,13 @@
 /*   By: abita <abita@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 18:06:49 by abita             #+#    #+#             */
-/*   Updated: 2025/11/21 18:06:54 by abita            ###   ########.fr       */
+/*   Updated: 2025/11/21 21:11:55 by abita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-static void	meal_check(t_philo *philo, int i)
-{
-	if (philo->data->number_of_times_each_philosopher_must_eat != -1
-		&& philo[i].meals_count >= 
-		philo->data->number_of_times_each_philosopher_must_eat)
-	{
-		pthread_mutex_lock(&philo->data->printing);
-		philo->data->stop_flag = true;
-		pthread_mutex_unlock(&philo->data->printing);
-		// break ;
-	}
-}
-
-static void	*start_monitoring(void *arg)
+static void *start_monitoring(void *arg)
 {
 	t_philo	*philo;
 	int		i;
@@ -36,7 +23,9 @@ static void	*start_monitoring(void *arg)
 	{
 		if (philo->data->stop_flag)
 			break ;
-		meal_check(philo, i);
+		if (philo->data->number_of_times_each_philosopher_must_eat != -1
+			&& philo[i].meals_count == philo->data->number_of_times_each_philosopher_must_eat)
+			break ;
 		pthread_mutex_lock(&philo->data->dead_mutex);
 		if (getmillisec() - philo[i].last_meal > philo->data->time.time_to_die)
 		{
@@ -45,43 +34,25 @@ static void	*start_monitoring(void *arg)
 			break ;
 		}
 		pthread_mutex_unlock(&philo->data->dead_mutex);
-		i = (i) % philo->data->num_of_philos;
-		usleep(1000);
+		i = (i + 1) % philo->data->num_of_philos;
 	}
 	return (NULL);
 }
 
-void	init_waiter_thread(t_data *data, t_philo *philo)
+void	init_join_waiter_thread(t_data *data, t_philo *philo)
 {
 	int	j;
 
 	if (pthread_create(&philo->waiter_thread, NULL, &start_monitoring,
 			philo) != 0)
 	{
+		philo->data->stop_flag = true;
 		print_error("Failed to create waiter thread.\n");
 		j = 0;
 		while (j < data->num_of_philos)
-		{
-			pthread_join(philo[j].philo_thread, NULL);
-			j++;
-		}
-		free(philo);
+			pthread_join(philo[j++].philo_thread, NULL);
+		return ;
 	}
-}
-
-void	join_waiter_thread(t_data *data, t_philo *philo)
-{
-	int	j;
-
 	if (pthread_join(philo->waiter_thread, NULL))
-	{
 		print_error("Failed to join waiter thread.\n");
-		j = 0;
-		while (j < data->num_of_philos)
-		{
-			pthread_join(philo[j].philo_thread, NULL);
-			j++;
-		}
-		free(philo);
-	}
 }
